73e01335eaa9705cc4bd0eb92a889f211b878a4a
amanda-thrive <amanda@thrivemg.com>
2014-02-20 10:01:40 -0600

    initialize rails with `rails new . -T --database=postgresql`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubydoo::Application.config.secret_key_base = '5d6285eae95c80ae13c7268a33def4cd51cc215b30f3d0a50b5fa08693a1e53f2a2868b4bc9ef35385a39ac8611e9f7bb0cd5ea85ead12354b5e46aea2656948'
