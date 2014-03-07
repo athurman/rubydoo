d513ff2a7169cd435578fde4f66ef7946ab42c20
athurman <athurman215@gmail.com>
2014-03-07 06:47:57 -0600

    create scores model, create table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303165027) do
+ActiveRecord::Schema.define(version: 20140307124037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20140303165027) do
     t.string   "role"
     t.text     "temperament"
     t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "scores", force: true do |t|
+    t.integer  "answer_id"
+    t.integer  "breed_id"
+    t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
