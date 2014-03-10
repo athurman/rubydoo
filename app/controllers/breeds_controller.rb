394c5eb59573346a8ea2b0b5124db48fdc546857
athurman <athurman215@gmail.com>
2014-03-10 11:30:03 -0500

    Score and locate top five breeds on survey submit

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,19 +1,28 @@
 class BreedsController < ApplicationController
 
   def index
-    params[:size]
-    params[:energy]
-    params[:time]
-    params[:animal_friendly]
-    params[:role]
-    params[:grooming]
-    params[:space]
-    # i = 0
-    # all_breeds = Breed.all
-    # all_breeds.each do
-    #   score = Breed.generate_score([], all_breeds[i].id)
-    # end
-    # output.sort_by {|k, v| v}.reverse
+    selected_answer_array = [params[:size], params[:energy], params[:time], params[:animal_friendly], params[:role], params[:grooming], params[:space]]
+    top_five_breeds = []
+    i = 0
+    all_breeds = Breed.all
+    output = {}
+    all_breeds.each do
+      score = Breed.generate_score(selected_answer_array, all_breeds[i].id)
+      output[all_breeds[i].id] = score
+      i += 1
+    end
+
+    output = output.sort_by {|k, v| v}.reverse
+    j = 0
+    5.times do
+      id = output[j][0]
+      breed = Breed.find_by(id: id)
+      top_five_breeds << breed
+      j += 1
+    end
+
+
+    @top_five_breeds = top_five_breeds
   end
 
   def find
