6ceb3049ae958ed186c00992b35bb53fae8fb571
athurman <athurman215@gmail.com>
2014-03-11 12:32:31 -0500

    create API and Json Parser methods to retrieve shelterdogs from PetFinder

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -49,4 +49,35 @@ class JsonParser
     end
   end
 
+  def self.parse_shelter_dogs(response)
+    dogs = []
+    parsed = JSON.parse(response.body)
+    puts parsed["petfinder"]["pets"]["pet"]
+    parsed["petfinder"]["pets"]["pet"].each do |pet_obj|
+      shelter_dog = {}
+      shelter_dog["name"] = pet_obj["name"]["$t"]
+      shelter_dog["sex"] = pet_obj["sex"]["$t"]
+      shelter_dog["age"] = pet_obj["age"]["$t"]
+      shelter_dog["address"] = pet_obj["contact"]["address1"]["$t"]
+      shelter_dog["city"] = pet_obj["contact"]["city"]["$t"] unless pet_obj["contact"]["city"].nil?
+      shelter_dog["state"] = pet_obj["contact"]["state"]["$t"] unless pet_obj["contact"]["state"].nil?
+      shelter_dog["zipcode"] = pet_obj["contact"]["zip"]["$t"] unless pet_obj["contact"]["zip"].nil?
+      shelter_dog["contact_name"] = pet_obj["contact"]["name"]["$t"] unless pet_obj["contact"]["name"].nil?
+      shelter_dog["email"] = pet_obj["contact"]["email"]["$t"] unless pet_obj["contact"]["email"].nil?
+      shelter_dog["contact_phone"] = pet_obj["contact"]["phone"]["$t"] unless pet_obj["contact"]["phone"].nil?
+
+      shelter_dog["description"] = pet_obj["description"]["$t"]
+      shelter_dog["image"] = pet_obj["media"]["photos"]["photo"][0]["$t"]
+      shelter_dog["breeds"] = []
+      if pet_obj["breeds"]["breed"].is_a?(Array)
+        pet_obj["breeds"]["breed"].each do |breed_hash|
+          shelter_dog["breeds"] << breed_hash["$t"]
+        end
+      else
+        shelter_dog["breeds"] << pet_obj["breeds"]["breed"]["$t"]
+      end
+      dogs << shelter_dog
+    end
+    dogs
+  end
 end
--- lib/petfinder.rb
+++ lib/petfinder.rb
@@ -20,4 +20,12 @@ class PetFinder
     response = HTTParty.get(BASE_URL + 'shelter.find?key=' + ENV['PETFINDER_API_KEY'] + '&location=' + zip + '&format=json&token=' + token + '&sig=' + secret)
     JsonParser.parse_shelters(response)
   end
+
+  def self.retrieve_shelter_dogs(zip_code, breed_name)
+    token = PetFinder.get_token
+    zip = zip_code.to_s
+    secret = Digest::MD5.hexdigest(ENV['PETFINDER_SECRET'] + 'key=' + ENV['PETFINDER_API_KEY'] + '&location=' + zip + '&animal=dog&breed=' + breed_name + '&count=40&format=json&token=' + token)
+    response = HTTParty.get(BASE_URL + 'pet.find?key=' + ENV['PETFINDER_API_KEY'] + '&location=' + zip + '&animal=dog&breed=' + breed_name + '&count=40&format=json&token=' + token + '&sig=' + secret)
+    JsonParser.parse_shelter_dogs(response)
+  end
 end
