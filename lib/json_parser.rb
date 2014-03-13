7cf93003dd13d1c8be2e0ed4f840bdd8853e80c4
athurman <athurman215@gmail.com>
2014-03-12 21:46:02 -0500

    fix bug if only one dog is returned from api.

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -55,30 +55,57 @@ class JsonParser
   def self.parse_shelter_dogs(response)
     dogs = []
     parsed = JSON.parse(response.body)
-    parsed["petfinder"]["pets"]["pet"].each do |pet_obj|
-      shelter_dog = {}
-      shelter_dog["name"] = pet_obj["name"]["$t"]
-      shelter_dog["sex"] = pet_obj["sex"]["$t"]
-      shelter_dog["age"] = pet_obj["age"]["$t"]
-      shelter_dog["address"] = pet_obj["contact"]["address1"]["$t"]
-      shelter_dog["city"] = pet_obj["contact"]["city"]["$t"] unless pet_obj["contact"]["city"].nil?
-      shelter_dog["state"] = pet_obj["contact"]["state"]["$t"] unless pet_obj["contact"]["state"].nil?
-      shelter_dog["zipcode"] = pet_obj["contact"]["zip"]["$t"] unless pet_obj["contact"]["zip"].nil?
-      shelter_dog["contact_name"] = pet_obj["contact"]["name"]["$t"] unless pet_obj["contact"]["name"].nil?
-      shelter_dog["email"] = pet_obj["contact"]["email"]["$t"] unless pet_obj["contact"]["email"].nil?
-      shelter_dog["contact_phone"] = pet_obj["contact"]["phone"]["$t"] unless pet_obj["contact"]["phone"].nil?
+    if parsed["petfinder"]["pets"]["pet"].is_a?(Array)
+      parsed["petfinder"]["pets"]["pet"].each do |pet_obj|
+        shelter_dog = {}
+        shelter_dog["name"] = pet_obj["name"]["$t"]
+        shelter_dog["sex"] = pet_obj["sex"]["$t"]
+        shelter_dog["age"] = pet_obj["age"]["$t"]
+        shelter_dog["address"] = pet_obj["contact"]["address1"]["$t"]
+        shelter_dog["city"] = pet_obj["contact"]["city"]["$t"] unless pet_obj["contact"]["city"].nil?
+        shelter_dog["state"] = pet_obj["contact"]["state"]["$t"] unless pet_obj["contact"]["state"].nil?
+        shelter_dog["zipcode"] = pet_obj["contact"]["zip"]["$t"] unless pet_obj["contact"]["zip"].nil?
+        shelter_dog["contact_name"] = pet_obj["contact"]["name"]["$t"] unless pet_obj["contact"]["name"].nil?
+        shelter_dog["email"] = pet_obj["contact"]["email"]["$t"] unless pet_obj["contact"]["email"].nil?
+        shelter_dog["contact_phone"] = pet_obj["contact"]["phone"]["$t"] unless pet_obj["contact"]["phone"].nil?
 
-      shelter_dog["description"] = pet_obj["description"]["$t"]
-      unless pet_obj["media"]["photos"].nil?
-        shelter_dog["image"] = pet_obj["media"]["photos"]["photo"].first["$t"]
+        shelter_dog["description"] = pet_obj["description"]["$t"]
+        unless pet_obj["media"]["photos"].nil?
+          shelter_dog["image"] = pet_obj["media"]["photos"]["photo"].first["$t"]
+        end
+        shelter_dog["breeds"] = []
+        if pet_obj["breeds"]["breed"].is_a?(Array)
+          pet_obj["breeds"]["breed"].each do |breed_hash|
+            shelter_dog["breeds"] << breed_hash["$t"]
+          end
+        else
+          shelter_dog["breeds"] << pet_obj["breeds"]["breed"]["$t"]
+        end
+        dogs << shelter_dog
+      end
+    else
+      shelter_dog = {}
+      shelter_dog["name"] = parsed["petfinder"]["pets"]["pet"]["name"]["$t"]
+      shelter_dog["sex"] = parsed["petfinder"]["pets"]["pet"]["sex"]["$t"]
+      shelter_dog["age"] = parsed["petfinder"]["pets"]["pet"]["age"]["$t"]
+      shelter_dog["address"] = parsed["petfinder"]["pets"]["pet"]["contact"]["address1"]["$t"]
+      shelter_dog["city"] = parsed["petfinder"]["pets"]["pet"]["contact"]["city"]["$t"] unless parsed["petfinder"]["pets"]["pet"]["contact"]["city"].nil?
+      shelter_dog["state"] = parsed["petfinder"]["pets"]["pet"]["contact"]["state"]["$t"] unless parsed["petfinder"]["pets"]["pet"]["contact"]["state"].nil?
+      shelter_dog["zipcode"] = parsed["petfinder"]["pets"]["pet"]["contact"]["zip"]["$t"] unless parsed["petfinder"]["pets"]["pet"]["contact"]["zip"].nil?
+      shelter_dog["contact_name"] = parsed["petfinder"]["pets"]["pet"]["contact"]["name"]["$t"] unless parsed["petfinder"]["pets"]["pet"]["contact"]["name"].nil?
+      shelter_dog["email"] = parsed["petfinder"]["pets"]["pet"]["contact"]["email"]["$t"] unless parsed["petfinder"]["pets"]["pet"]["contact"]["email"].nil?
+      shelter_dog["contact_phone"] = parsed["petfinder"]["pets"]["pet"]["contact"]["phone"]["$t"] unless parsed["petfinder"]["pets"]["pet"]["contact"]["phone"].nil?
+      shelter_dog["description"] = parsed["petfinder"]["pets"]["pet"]["description"]["$t"]
+      unless parsed["petfinder"]["pets"]["pet"]["media"]["photos"].nil?
+        shelter_dog["image"] = parsed["petfinder"]["pets"]["pet"]["media"]["photos"]["photo"].first["$t"]
       end
       shelter_dog["breeds"] = []
-      if pet_obj["breeds"]["breed"].is_a?(Array)
-        pet_obj["breeds"]["breed"].each do |breed_hash|
+      if parsed["petfinder"]["pets"]["pet"]["breeds"]["breed"].is_a?(Array)
+        parsed["petfinder"]["pets"]["pet"]["breeds"]["breed"].each do |breed_hash|
           shelter_dog["breeds"] << breed_hash["$t"]
         end
       else
-        shelter_dog["breeds"] << pet_obj["breeds"]["breed"]["$t"]
+        shelter_dog["breeds"] << parsed["petfinder"]["pets"]["pet"]["breeds"]["breed"]["$t"]
       end
       dogs << shelter_dog
     end
